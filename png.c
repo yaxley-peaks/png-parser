8395801407f46de657b443ecf0c07bf13df5a42c
yaxley-peaks <epiclycoolgaemer@gmail.com>
2022-01-04 16:01:35 +0530

    more stuff

--- png.c
+++ png.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include<stdbool.h>
 
 #define PNG_SIG_CAP 8
 const uint8_t exp_sig[PNG_SIG_CAP] = {137, 80, 78, 71, 13, 10, 26, 10};
@@ -16,7 +17,8 @@ void read_bytes_or_panic(FILE *file, void *buf, size_t buf_cap) {
             fprintf(stderr, "ERROR: Could not read PNG header: %s\n", strerror(errno));
             exit(1);
         } else if (feof(file)) {
-            fprintf(stderr, "ERROR: Could not read PNG header: Reached end of file");
+            fprintf(stderr, "ERROR: Could not read %zu bytes: Reached end of file\n", buf_cap);
+            exit(1);
         } else {
             assert(0 && "unreacheable");
         }
@@ -27,10 +29,14 @@ void print_bytes(uint8_t *buf, size_t buf_cap) {
         printf("%u ", buf[i]);
     }
     printf("\n");
+    for (size_t i = 0; i < buf_cap; i++) {
+        printf("%c ", buf[i]);
+    }
+    printf("\n");
 }
 
 void reverse_bytes(void *buf0, size_t buf_cap) {
-    uint8_t* buf = buf0;
+    uint8_t *buf = buf0;
     for (size_t i = 0; i < buf_cap / 2; ++i) {
         uint8_t t = buf[i];
         buf[i] = buf[buf_cap - i - 1];
@@ -64,14 +70,38 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    print_bytes(sig, PNG_SIG_CAP);
-
     //32 bit int for chunk size
     //the bytes are reversed
-    uint32_t chunk_sz;
-    read_bytes_or_panic(input_file, &chunk_sz, sizeof(chunk_sz));
-    reverse_bytes(&chunk_sz, sizeof(chunk_sz));
-    printf("chunk size: %u\n", chunk_sz);
+    //size of data only. not the whole chunk
+    //with metadata
+    bool quit = false;
+    while (!quit) {
+        uint32_t chunk_sz;
+        read_bytes_or_panic(input_file, &chunk_sz, sizeof(chunk_sz));
+        reverse_bytes(&chunk_sz, sizeof(chunk_sz));
+
+        uint8_t chunk_type[4];
+        read_bytes_or_panic(input_file, chunk_type, sizeof(chunk_type));
+        if(*(uint32_t*) chunk_type == 0x444E4549){
+            quit = true;
+        }
+
+        if (fseek(input_file, chunk_sz, SEEK_CUR) < 0) {
+            fprintf(stderr, "ERROR: Could not skip chunk: %s\n", strerror(errno));
+            exit(1);
+        }
+
+        uint32_t chunk_crc;
+        read_bytes_or_panic(input_file, &chunk_crc, sizeof(chunk_crc));
+
+        printf("chunk size: %u\n", chunk_sz);
+        printf("chunk type: %.*s (0x%08X) \n",
+               (int)sizeof(chunk_type),
+               chunk_type,
+               *(uint32_t *)chunk_type);
+        printf("chunk crc 0x%08X\n", chunk_crc);
+        printf("----------\n");
+    }
     fclose(input_file);
     return 0;
 }
